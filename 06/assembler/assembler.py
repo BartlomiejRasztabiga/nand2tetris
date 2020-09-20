08668602ebad6266008d1d80757c0f715f3fc127
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-20 21:35:41 +0200

    Add concept of Parser

--- 06/assembler/assembler.py
+++ 06/assembler/assembler.py
@@ -1,3 +1,5 @@
+import string
+
 symbols = {
     'R0': 0,
     'R1': 1,
@@ -24,30 +26,46 @@ symbols = {
     'THAT': 4
 }
 
-def delete_comment(line):
-    comment_start_index = line.find('//')
-    if comment_start_index == -1:
-        return line
-    return line[:comment_start_index]
 
-def update_symbols(lines):
-    for line in lines:
-        line = delete_comment(line)
-        # print(line)
-        if len(line) == 0:
-            continue
-        else:
-            # pass
-            print(line)
-        
+class Parser():
+    def parse(self, lines):
+        parsed = []
+        lines = self.clear_lines(lines)
+
+        print(lines)
+
+        return lines
+
+    def clear_lines(self, lines):
+        return [x for x in map(self.clear_line, lines) if x is not None]
+
+    def clear_line(self, line):
+        s = "".join(self.delete_comment(line).split())
+        n = len(s)
+        return s if n != 0 else None
+
+    def delete_comment(self, line):
+        comment_start_index = line.find('//')
+        if comment_start_index == -1:
+            return line
+        return line[:comment_start_index]
+
+    def is_label(self, line):
+        pass
+
+
+def update_symbols(parser, lines):
+    parser.parse(lines)
+
 
 def main():
     in_file = 'in.asm'
     out_file = 'out.hack'
+    parser = Parser()
 
     with open(in_file, 'r') as file:
         lines = list(map(lambda x: x.strip(), file.readlines()))
-        update_symbols(lines)
+        update_symbols(parser, lines)
 
 
 if __name__ == '__main__':
