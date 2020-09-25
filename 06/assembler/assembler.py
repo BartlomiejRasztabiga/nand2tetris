3a67a0817d75f69e78b59cbd437a5c3d1d939280
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-25 20:57:27 +0200

    Add SymbolTable class

--- 06/assembler/assembler.py
+++ 06/assembler/assembler.py
@@ -2,31 +2,46 @@ import sys
 import string
 import re
 
-symbols = {
-    'R0': 0,
-    'R1': 1,
-    'R2': 2,
-    'R3': 3,
-    'R4': 4,
-    'R5': 5,
-    'R6': 6,
-    'R7': 7,
-    'R8': 8,
-    'R9': 9,
-    'R10': 10,
-    'R11': 11,
-    'R12': 12,
-    'R13': 13,
-    'R14': 14,
-    'R15': 15,
-    'SCREEN': 16384,
-    'KBD': 24576,
-    'SP': 0,
-    'LCL': 1,
-    'ARG': 2,
-    'THIS': 3,
-    'THAT': 4
-}
+
+class SymbolTable():
+    def __init__(self):
+        self.symbols = {
+            'R0': 0,
+            'R1': 1,
+            'R2': 2,
+            'R3': 3,
+            'R4': 4,
+            'R5': 5,
+            'R6': 6,
+            'R7': 7,
+            'R8': 8,
+            'R9': 9,
+            'R10': 10,
+            'R11': 11,
+            'R12': 12,
+            'R13': 13,
+            'R14': 14,
+            'R15': 15,
+            'SCREEN': 16384,
+            'KBD': 24576,
+            'SP': 0,
+            'LCL': 1,
+            'ARG': 2,
+            'THIS': 3,
+            'THAT': 4
+        }
+        self.nextAvailableValue = 16
+
+    def getSymbolValue(self, symbolName):
+        if symbolName not in self.symbols:
+            return None
+        return self.symbols[symbolName]
+
+    def addSymbol(self, symbolName):
+        if symbolName in self.symbols:
+            return  # should throw exception
+        self.symbols[symbolName] = self.nextAvailableValue
+        self.nextAvailableValue += 1
 
 
 class Label():
@@ -118,7 +133,7 @@ def main():
         print("No input file supplied")
         return
 
-    in_file = sys.argv[1]        
+    in_file = sys.argv[1]
     out_file = in_file.replace('.asm', '.hack')
 
     assembler = Assembler(in_file, out_file)
